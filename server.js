b58c6f5aaa7ed593f2ce24f12b0d394b8413ab33
Offiahchiaz <offiahchiaz@gmail.com>
2019-02-25 23:48:49 +0100

    App structured using mvc architecture

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('express-flash');
+const passport = require('passport');
 
 const app = express();
 const port = process.env.PORT || 7000;
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.json({
